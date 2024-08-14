d4a5a35aebfc5179b888bf5ab5212874e7020d98
dsantillan2 <dsantillan2ta@gmail.com>
2024-08-14 17:11:32 -0500

    repositorio de parametros generales y libro diario

--- src/main/java/com/example/proyecto_citas_medicas/repository/GeneralParameterRepository.java
+++ src/main/java/com/example/proyecto_citas_medicas/repository/GeneralParameterRepository.java
@@ -1,6 +1,6 @@
-package repository;
+package com.example.proyecto_citas_medicas.repository;
 
-import entities.GeneralParameter;
+import com.example.proyecto_citas_medicas.entities.GeneralParameter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
