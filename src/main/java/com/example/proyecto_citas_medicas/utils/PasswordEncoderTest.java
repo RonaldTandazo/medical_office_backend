028d9858e4b4e8cbe7c76070e3a09afea895b723
dsantillan2 <dsantillan2ta@gmail.com>
2024-08-14 17:13:43 -0500

    prueba para la codificacion de contraseñas

--- src/main/java/com/example/proyecto_citas_medicas/utils/PasswordEncoderTest.java
+++ src/main/java/com/example/proyecto_citas_medicas/utils/PasswordEncoderTest.java
@@ -1,4 +1,12 @@
 package com.example.proyecto_citas_medicas.utils;
 
-public class sdsd {
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTest {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "1234"; // La contraseña sin cifrar
+        String encodedPassword = encoder.encode(rawPassword);
+        System.out.println("Encoded password: " + encodedPassword);
+    }
 }
