4cf963ae63e3745fa5de43ebf42483f61f3db980
dsantillan2 <dsantillan2ta@gmail.com>
2024-08-14 15:52:17 -0500

    controlador para las pruebas del login

--- src/main/java/com/example/proyecto_citas_medicas/controller/LoginController.java
+++ src/main/java/com/example/proyecto_citas_medicas/controller/LoginController.java
@@ -1,13 +1,26 @@
-package controller;
+package com.example.proyecto_citas_medicas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @GetMapping("/login")
     public String login() {
-        return "login"; // Retorna la vista login
+        // Método para manejar la visualización del formulario de login
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String authenticate(String username, String password) {
+        // Aquí puedes usar passwordEncoder para comparar contraseñas
+        return "home";
     }
 }
